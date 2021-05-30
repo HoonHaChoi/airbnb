f41a3367682ec0198f10c0fd895e32589e3ef5f8
hoonha choi <sosqsz2@naver.com>
2021-05-30 21:46:20 +0900

    feat: 숙소 이미지에 하트 추가 및  MapViewController 생성

---
+++ iOS/airBnb/airBnb/ViewController/Map/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  airBnb
+//
+//  Created by HOONHA CHOI on 2021/05/30.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
